904af1f08ca6c2dfe1806e32c80b11da019d0be1
choieastsea <choieastsea@gmail.com>
2022-05-24 17:46:11 +0900

    Set Basic Layout for application

--- app/src/main/java/com/lucyseven/clothmatchingservice/MainActivity.kt
+++ app/src/main/java/com/lucyseven/clothmatchingservice/MainActivity.kt
@@ -5,19 +5,22 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.lucyseven.clothmatchingservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private val db = FirebaseFirestore.getInstance()
-    lateinit var myAdapter:MyAdapter
+    lateinit var myAdapter: MyAdapter
     var itemList = arrayListOf<User>()
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()    //remove title bar
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initData()
+        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, HomeFragment()).commitAllowingStateLoss()
         initLayout()
     }
 
@@ -29,7 +32,7 @@ class MainActivity : AppCompatActivity() {
                 // 성공할 경우
                 itemList.clear()
                 for (document in result!!) {  // 가져온 문서들은 result에 들어감
-                    Log.i("asdfasdf",document["email"].toString())
+                    Log.i("asdfasdf", document["email"].toString())
                     val item = User(document["email"].toString(), document["pw"].toString())
                     itemList.add(item)
                 }
@@ -37,34 +40,34 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private fun initRecyclerView() {
-        binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        myAdapter = MyAdapter(itemList)
-        Log.i("asdfasdf",itemList.toString())
-        myAdapter.itemClickListener = object : MyAdapter.OnItemClickListener{
-            override fun onItemClick(data: User) {
-                //adapter.notifyDataSetChanged()  // 변경 정보 등록 가능
-            }
-        }
-        binding.recyclerView.adapter = myAdapter
-    }
 
     private fun initLayout() {
         binding.apply {
-            button.setOnClickListener {
-                val data = hashMapOf(
-                    "email" to emailInput.text.toString(),
-                    "pw" to pwInput.text.toString()
-                )
-                db.collection("User").add(data)
-                    .addOnSuccessListener {
-                        // 성공할 경우
-                        Toast.makeText(this@MainActivity, "데이터가 추가되었습니다", Toast.LENGTH_SHORT).show()
+            bottomNavigation.setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.page_1 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, HomeFragment()).commitAllowingStateLoss()
+                        true
                     }
-            }
-            show.setOnClickListener {
-                initRecyclerView()
+                    R.id.page_2 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, CommuFragment()).commitAllowingStateLoss()
+                        true
+                    }
+                    R.id.page_3 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, LinkFragment()).commitAllowingStateLoss()
+                        true
+                    }
+                    R.id.page_4 -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, SettingFragment()).commitAllowingStateLoss()
+                        true
+                    }
+                    else -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.linearLayout, HomeFragment()).commitAllowingStateLoss()
+                        false
+                    }
+                }
             }
         }
+
     }
 }
