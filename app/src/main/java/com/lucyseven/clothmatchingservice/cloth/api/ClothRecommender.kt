dc128b09cce3618398142901c4f1e98284f5b2b6
Sangin Lee <tkddls0614@gmail.com>
2022-06-06 16:53:56 +0900

    옷 아이콘 추가 / gradle파일 업데이트

--- app/src/main/java/com/lucyseven/clothmatchingservice/cloth/api/ClothRecommender.kt
+++ app/src/main/java/com/lucyseven/clothmatchingservice/cloth/api/ClothRecommender.kt
@@ -4,37 +4,4 @@ interface ClothRecommender {
     fun recommend(minTemp: Int, maxTemp: Int): List<Cloth>
 }
 
-enum class Cloth {
-    LONG_PADDING,
-    PADDING,
-    SCARF,
-    GLOVES,
-    FLEECE_PANTS,
-    WINTER_COAT,
-    LIGHT_PADDING,
-    LEATHER_JACKET,
-    LONG_UNDERWEAR,
-    TRENCH_COAT,
-    JUMPER,
-    THIN_COAT,
-    JACKET,
-    THICK_CARDIGAN,
-    THICK_NEAT,
-    NEAT,
-    CARDIGAN,
-    HOOD,
-    MANTOMAN,
-    THIN_JACKET,
-    COTTON_PANTS,
-    SLACKS,
-    LONG_T_SHIRT,
-    SHIRT,
-    ONE_PIECE,
-    THIN_LONG_T_SHIRT,
-    THIN_SHIRT,
-    T_SHIRT,
-    SHORT_PANTS,
-    SLEEVELESS,
-    LINEN
-}
-
+data class Cloth(val iconId: Int, val name: String)
--- app/src/main/java/com/lucyseven/clothmatchingservice/cloth/impl/ClothDataImpl.kt
+++ app/src/main/java/com/lucyseven/clothmatchingservice/cloth/impl/ClothDataImpl.kt
@@ -1,5 +1,6 @@
 package com.lucyseven.clothmatchingservice.cloth.impl
 
+import com.lucyseven.clothmatchingservice.R
 import com.lucyseven.clothmatchingservice.cloth.api.Cloth
 import com.lucyseven.clothmatchingservice.cloth.api.ClothRecommender
 
@@ -10,71 +11,95 @@ class ClothDataImpl : ClothRecommender {
         when ((minTemp + maxTemp) / 2) {
             in -70..-10 ->
                 arrayListOf(
-                    Cloth.LONG_PADDING,
-                    Cloth.FLEECE_PANTS
+                    Cloth(R.drawable.winter_padding, "롱패딩 / 겨울패딩"),
+                    Cloth(R.drawable.fleece_pants, "내복"),
+                    Cloth(R.drawable.winter_pants, "겨울면바지"),
+                    Cloth(R.drawable.gloves, "장갑"),
+                    Cloth(R.drawable.winter_scarf, "목도리")
                 )
             in -9..4 ->
                 arrayListOf(
-                    Cloth.LONG_PADDING,
-                    Cloth.PADDING,
-                    Cloth.WINTER_COAT,
-                    Cloth.SCARF,
-                    Cloth.GLOVES,
-                    Cloth.FLEECE_PANTS
+                    Cloth(R.drawable.winter_padding, "롱패딩 / 겨울패딩"),
+                    Cloth(R.drawable.padding, "패딩"),
+                    Cloth(R.drawable.winter_coat, "겨울코트"),
+                    Cloth(R.drawable.fleece_pants, "내복"),
+                    Cloth(R.drawable.winter_pants, "겨울면바지"),
+                    Cloth(R.drawable.gloves, "장갑"),
+                    Cloth(R.drawable.winter_scarf, "목도리")
                 )
             in 5..8 ->
                 arrayListOf(
-                    Cloth.WINTER_COAT,
-                    Cloth.LIGHT_PADDING,
-                    Cloth.LEATHER_JACKET,
-                    Cloth.LONG_UNDERWEAR,
+                    Cloth(R.drawable.winter_coat, "겨울코트"),
+                    Cloth(R.drawable.padding, "패딩"),
+                    Cloth(R.drawable.leather_jacket, "가죽자켓"),
+                    Cloth(R.drawable.jacket, "자켓"),
+                    Cloth(R.drawable.sweater, "스웨터"),
+                    Cloth(R.drawable.winter_pants, "겨울면바지")
                 )
             in 9..11 ->
                 arrayListOf(
-                    Cloth.LIGHT_PADDING,
-                    Cloth.LEATHER_JACKET,
-                    Cloth.TRENCH_COAT,
-                    Cloth.JUMPER,
-                    Cloth.THIN_COAT,
+                    Cloth(R.drawable.padding, "가벼운패딩"),
+                    Cloth(R.drawable.leather_jacket, "가죽자켓"),
+                    Cloth(R.drawable.thin_coat, "트렌치코트"),
+                    Cloth(R.drawable.thin_coat, "얇은 코트"),
+                    Cloth(R.drawable.leather_jacket, "가죽자켓"),
+                    Cloth(R.drawable.jacket, "자켓"),
+                    Cloth(R.drawable.sweater, "스웨터"),
+                    Cloth(R.drawable.cotton_pants, "면바지"),
+                    Cloth(R.drawable.jeans, "청바지")
                 )
             in 12..15 ->
                 arrayListOf(
-                    Cloth.THIN_COAT,
-                    Cloth.JUMPER,
-                    Cloth.JACKET,
-                    Cloth.THICK_CARDIGAN,
-                    Cloth.THICK_NEAT,
+                    Cloth(R.drawable.thin_coat, "트렌치코트"),
+                    Cloth(R.drawable.thin_coat, "얇은 코트"),
+                    Cloth(R.drawable.leather_jacket, "가죽자켓"),
+                    Cloth(R.drawable.jacket, "자켓"),
+                    Cloth(R.drawable.sweater, "스웨터"),
+                    Cloth(R.drawable.cardigan, "두꺼운 가디건"),
+                    Cloth(R.drawable.cotton_pants, "면바지"),
+                    Cloth(R.drawable.jeans, "청바지")
+
                 )
             in 15..19 ->
                 arrayListOf(
-                    Cloth.NEAT,
-                    Cloth.CARDIGAN,
-                    Cloth.HOOD,
-                    Cloth.MANTOMAN,
-                    Cloth.THIN_JACKET,
-                    Cloth.COTTON_PANTS,
-                    Cloth.SLACKS
+                    Cloth(R.drawable.jacket, "얇은 자켓"),
+                    Cloth(R.drawable.sweater, "스웨터"),
+                    Cloth(R.drawable.cardigan, "얇은 가디건"),
+                    Cloth(R.drawable.hoodie, "후드티"),
+                    Cloth(R.drawable.mantoman, "맨투맨"),
+                    Cloth(R.drawable.cotton_pants, "면바지"),
+                    Cloth(R.drawable.jeans, "청바지"),
+                    Cloth(R.drawable.slacks, "슬랙스")
                 )
             in 20..22 ->
                 arrayListOf(
-                    Cloth.LONG_T_SHIRT,
-                    Cloth.HOOD,
-                    Cloth.SHIRT,
-                    Cloth.ONE_PIECE
+                    Cloth(R.drawable.long_sleeve, "긴팔티"),
+                    Cloth(R.drawable.shirt, "와이셔츠"),
+                    Cloth(R.drawable.hoodie, "후드티"),
+                    Cloth(R.drawable.mantoman, "맨투맨"),
+                    Cloth(R.drawable.cotton_pants, "면바지"),
+                    Cloth(R.drawable.jeans, "청바지"),
+                    Cloth(R.drawable.slacks, "슬랙스")
                 )
             in 23..26 ->
                 arrayListOf(
-                    Cloth.THIN_LONG_T_SHIRT,
-                    Cloth.THIN_SHIRT,
-                    Cloth.T_SHIRT,
-                    Cloth.SHORT_PANTS
+                    Cloth(R.drawable.tshirt, "반팔티"),
+                    Cloth(R.drawable.long_sleeve, "얇은 긴팔티"),
+                    Cloth(R.drawable.summer_shirts, "여름 셔츠"),
+                    Cloth(R.drawable.linen, "여름바지"),
+                    Cloth(R.drawable.onepiece, "원피스"),
+                    Cloth(R.drawable.skirt, "치마"),
+                    Cloth(R.drawable.slacks, "슬랙스"),
+                    Cloth(R.drawable.shorts_2, "반바지")
                 )
             else ->
                 arrayListOf(
-                    Cloth.SLEEVELESS,
-                    Cloth.LINEN,
-                    Cloth.SHORT_PANTS,
-                    Cloth.T_SHIRT
+                    Cloth(R.drawable.shirt, "반팔티"),
+                    Cloth(R.drawable.sleeveless, "민소매"),
+                    Cloth(R.drawable.onepiece, "원피스"),
+                    Cloth(R.drawable.short_onepiece, "짧은 원피스"),
+                    Cloth(R.drawable.shorts, "짧은 반바지"),
+                    Cloth(R.drawable.shorts_2, "반바지")
                 )
         }
 }
